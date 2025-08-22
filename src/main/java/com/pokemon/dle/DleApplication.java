331cb3944ad89c848acf0807558eaaddd9b29c37
DouglasAVSantos <104694849+DouglasAVSantos@users.noreply.github.com>
2025-08-22 17:05:21 -0300

    inicial commit

---
+++ src/main/java/com/pokemon/dle/DleApplication.java
@@ -0,0 +1,13 @@
+package com.pokemon.dle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pokemon/dle/DleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokemon.dle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
